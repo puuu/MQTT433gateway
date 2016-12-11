19f987e6235b67d7a782cf1bb1ef994509f61bf4
puuu <puuu@users.noreply.github.com>
2016-12-11 21:41:23 +0900

    src/Heartbeat: New sub class that allows flashing of the LED.

---
+++ src/Heartbeat/HeartbeatFlashing.h
@@ -0,0 +1,48 @@
+/*
+  HeartbeatFlashing - Library for flashing LED
+  Project home: https://github.com/puuu/MQTT433gateway/
+
+  The MIT License (MIT)
+
+  Copyright (c) 2016 Puuu
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef HeartbeatFlashing_h
+#define HeartbeatFlashing_h
+
+#include <Arduino.h>
+#include <Ticker.h>
+#include "Heartbeat.h"
+
+
+class HeartbeatFlashing : public Heartbeat {
+ public:
+  HeartbeatFlashing(int pin, int interval=100);
+  virtual void off();
+  void flash(unsigned int onMilliseconds);
+ protected:
+  boolean _flashing;
+  Ticker _ticker;
+};
+
+#endif
