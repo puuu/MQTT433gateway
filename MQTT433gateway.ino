accb15345d81e3d1e8ae502757811f943a3bd220
tvch <tobias@verbree.ch>
2017-03-16 16:51:54 +0100

    hide ESP_xxxxx SSID

--- MQTT433gateway.ino
+++ MQTT433gateway.ino
@@ -80,6 +80,10 @@ void setupWifi() {
   Serial.print(F("Connecting to "));
   Serial.println(ssid);
 
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
