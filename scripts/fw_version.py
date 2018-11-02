e818a313733dc1533a91db0baa9afe04a33f9f16
denxhun <34472201+denxhun@users.noreply.github.com>
2018-11-02 13:10:58 +0100

    scripts/fw_version.py: make fw version extraction windows compatible

---
+++ scripts/fw_version.py
@@ -0,0 +1,14 @@
+"""MQTT433gateway git firmware version extractor
+
+Project home: https://github.com/puuu/MQTT433gateway/
+"""
+import subprocess
+Import("env")
+
+framework_version = '-DFIRMWARE_VERSION="' + subprocess.check_output(['git', 'describe', '--abbrev=8', '--dirty', '--always', '--tags']).strip() + '"'
+
+env.Append(
+    BUILD_FLAGS=[
+        framework_version
+    ]
+)
