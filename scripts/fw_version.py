861f12bdab33423b31ecb2cb5d39512df7a7da34
puuu <puuu@users.noreply.github.com>
2018-11-06 08:53:26 +0100

    You are right, agree.
    Co-Authored-By: denxhun <34472201+denxhun@users.noreply.github.com>

--- scripts/fw_version.py
+++ scripts/fw_version.py
@@ -5,7 +5,7 @@ Project home: https://github.com/puuu/MQTT433gateway/
 import subprocess
 Import("env")
 
-framework_version = str("-DFRAMEWORK_VERSION=\\\"") + subprocess.check_output(['git', 'describe', '--abbrev=8', '--dirty', '--always', '--tags']).strip() + str("\\\"")
+framework_version = '-DFIRMWARE_VERSION="' + subprocess.check_output(['git', 'describe', '--abbrev=8', '--dirty', '--always', '--tags']).strip() + '"'
 
 env.Append(
   BUILD_FLAGS=[
