0b6d5d1b2aee3a33763c249f3acce0df2a904f0a
puuu <puuu@users.noreply.github.com>
2018-11-12 18:14:41 +0900

    scripts/fw_version.py: refactor to reusable functions

--- scripts/fw_version.py
+++ scripts/fw_version.py
@@ -5,10 +5,15 @@ Project home: https://github.com/puuu/MQTT433gateway/
 import subprocess
 Import("env")
 
-framework_version = '-DFIRMWARE_VERSION="' + subprocess.check_output(['git', 'describe', '--abbrev=8', '--dirty', '--always', '--tags']).strip() + '"'
+def get_fw_version():
+    return subprocess.check_output(['git', 'describe', '--abbrev=8', '--dirty',
+                                    '--always', '--tags']).strip()
+
+def gen_build_flag_macro(macro, value):
+    return "-D{name}='{definition}'".format(name=macro, definition=value)
 
 env.Append(
     BUILD_FLAGS=[
-        framework_version
+        gen_build_flag_macro('FIRMWARE_VERSION', get_fw_version())
     ]
 )
